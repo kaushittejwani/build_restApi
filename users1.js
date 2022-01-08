fbf9960c03fcab7077aed6b865f5977385feaa34
kaushittejwani <95282960+kaushittejwani@users.noreply.github.com>
2022-01-08 22:41:09 +0530

    Update users1.js

--- users1.js
+++ users1.js
@@ -2,22 +2,22 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const userSchema = new mongoose.Schema({
     ProfileName:{
-        type:String,
-        required:true,
-        validate(value){
-            if(!validator.isAlpha(value)){
-                 throw new error("Invalid name");
-            }
+                    type:String,
+                    required:true,
+                    validate(value){
+                     if(!validator.isAlpha(value)){
+                         throw new error("Invalid name");
+                }
         }
         
     },
     DOB:{
-        type:String,
-        required:true
+            type:String,
+            required:true
     },
     status:{
-          type:String,
-          default:"ACTIVE"
+              type:String,
+              default:"ACTIVE"
     }
 })
   const employee=new mongoose.model("users",userSchema);
